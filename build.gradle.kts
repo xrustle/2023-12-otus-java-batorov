5e237bdbff2959c48543615035a5449ec2abd423
Dmitry Batorov <batorov@dev.vtb.ru>
2024-01-22 14:45:52 +0300

    hw4: initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
 
 plugins {
     idea
     id("io.spring.dependency-management")
+    id("org.springframework.boot") apply false
+    id("name.remal.sonarlint") apply false
+    id("com.diffplug.spotless") apply false
 }
 
 idea {
@@ -24,11 +28,26 @@ allprojects {
     }
 
     val guava: String by project
+    val testcontainersBom: String by project
 
     apply(plugin = "io.spring.dependency-management")
     dependencyManagement {
+        imports {
+            mavenBom(BOM_COORDINATES)
+            mavenBom("org.testcontainers:testcontainers-bom:$testcontainersBom")
+        }
+
         dependencies {
             dependency("com.google.guava:guava:$guava")
+        }
+    }
+
+    configurations.all {
+        resolutionStrategy {
+            failOnVersionConflict()
+
+            force("com.google.code.findbugs:jsr305:3.0.2")
+            force("org.sonarsource.sslr:sslr-core:1.24.0.633")
         }
     }
 }
@@ -43,5 +62,22 @@ subprojects {
     tasks.withType<JavaCompile> {
         options.encoding = "UTF-8"
         options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing"))
+    }
+
+    apply<name.remal.gradle_plugins.sonarlint.SonarLintPlugin>()
+    apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        java {
+            palantirJavaFormat("2.38.0")
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging.showExceptions = true
+        reports {
+            junitXml.required.set(true)
+            html.required.set(true)
+        }
     }
 }
