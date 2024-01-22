4d490ca2a9f54db9dc900b8906d8a3b2d285de57
Dmitry Batorov <batorov@dev.vtb.ru>
2024-01-22 14:41:00 +0300

    refactor: hw1 formatting

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -2,15 +2,17 @@ package ru.otus;
 
 import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
-
 import java.util.List;
+import java.util.logging.Logger;
 
 public class HelloOtus {
+    static Logger logger = Logger.getLogger(HelloOtus.class.getName());
+
     public static void main(String... args) {
         List<Integer> list = List.of(1, 1, 1, 2, 2, 4, 4, 4, 4, 4);
         Multiset<Integer> wordsMultiset = HashMultiset.create();
 
         wordsMultiset.addAll(list);
-        System.out.println(wordsMultiset);
+        logger.info(wordsMultiset::toString);
     }
 }
