9d84222f7ac7aff459e8241196945c32eb08c0dc
Dmitry Batorov <batorov@dev.vtb.ru>
2024-01-24 22:05:00 +0300

    fix: use interface Deque instead of ArrayDeque class

--- hw04-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,18 +1,19 @@
 package homework;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class CustomerReverseOrder {
 
     // надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
 
-    ArrayDeque<Customer> customerStack = new ArrayDeque<>();
+    private final Deque<Customer> customerStack = new ArrayDeque<>();
 
     public void add(Customer customer) {
-        customerStack.addLast(customer);
+        customerStack.push(customer);
     }
 
     public Customer take() {
-        return customerStack.pollLast();
+        return customerStack.pop();
     }
 }
